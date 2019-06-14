91f856caa603f00306bc9d2d53c7e1a0380d44b3
KILLER.CODES <Killercodes@users.noreply.github.com>
2019-06-14 18:50:36 +0530

    Update TCP Client.py

--- Demo/Python3/TCP Client.py
+++ Demo/Python3/TCP Client.py
@@ -7,15 +7,11 @@ target_port = 80
 
 # create a socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 # connect the client
 client.connect((target_host,target_port))
-
 # send some data
-client.send("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
-
+data = "GET / HTTP/1.1\r\nHost: google.com\r\n\r\n"
+client.send(data.encode("utf-8"))
 # receive some data
-response = client.recv(4096)
-
-# printout
-print (response)
+response = client.recv(4096).decode("utf-8")
+print(response)
